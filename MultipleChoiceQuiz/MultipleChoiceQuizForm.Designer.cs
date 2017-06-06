8a7f7d1bce3e68466a6e63a02c970341e122fb4b
Kevin Ma <kbma.kevin@gmail.com>
2017-06-05 21:22:43 -0400

    added MultipleChoiceQuiz project to the solution to test the UserControl FourOptionsMCQUserControl.

---
+++ MultipleChoiceQuiz/MultipleChoiceQuizForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace MultipleChoiceQuiz
+{
+    partial class MultipleChoiceQuizForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MultipleChoiceQuizForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "MultipleChoiceQuizForm";
+            this.Text = "Multiple Choice Quiz";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ MultipleChoiceQuiz/MultipleChoiceQuizForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleChoiceQuiz
+{
+    public partial class MultipleChoiceQuizForm : Form
+    {
+        public MultipleChoiceQuizForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
