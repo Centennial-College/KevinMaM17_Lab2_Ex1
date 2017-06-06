da8ecb51040f15205078761ab1fd433c1cdf2693
Kevin Ma <kbma.kevin@gmail.com>
2017-06-05 21:19:29 -0400

    added properties to expose questionLbl, radioButtonAnswer1/2/3/4, questionGroupBox (title)

--- KevinMaM17_Lab2_Ex1/FourOptionsMCQUserControl.cs
+++ KevinMaM17_Lab2_Ex1/FourOptionsMCQUserControl.cs
@@ -16,5 +16,42 @@ namespace KevinMaM17_Lab2_Ex1
         {
             InitializeComponent();
         }
+
+        //property for setting and getting the question
+        public string QuestionLbl
+        {
+            get { return this.questionLbl.Text; }
+            set { this.questionLbl.Text = value; }
+        }
+
+        //properties for setting and getting the answers
+        public string Answer1
+        {
+            get { return this.radioButtonAnswer1.Text; }
+            set { this.radioButtonAnswer1.Text = value; }
+        }
+        public string Answer2
+        {
+            get { return this.radioButtonAnswer2.Text; }
+            set { this.radioButtonAnswer2.Text = value; }
+        }
+        public string Answer3
+        {
+            get { return this.radioButtonAnswer3.Text; }
+            set { this.radioButtonAnswer3.Text = value; }
+        }
+        public string Answer4
+        {
+            get { return this.radioButtonAnswer4.Text; }
+            set { this.radioButtonAnswer4.Text = value; }
+        }
+
+        //property for setting and getting the title of the group box
+        public string GroupBoxTitle
+        {
+            get { return this.questionGroupBox.Text; }
+            set { this.questionGroupBox.Text = value; }
+        }
+
     }
 }
